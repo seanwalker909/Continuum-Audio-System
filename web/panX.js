48f4251a1ce0366fc3338f802abd4d0ab417d063
Sean Walker <seanwalker909@gmail.com>
2018-11-02 16:18:16 -0500

    panx progress

--- web/panX.js
+++ web/panX.js
@@ -2,54 +2,45 @@ class panX extends AudioWorkletProcessor {
 
   // Custom AudioParams can be defined with this static getter.
   static get parameterDescriptors() {
-    return [{ name: 'panAudioParam', defaultValue: 0 }];
+    return [{ name: 'gain', defaultValue: 0 },
+    { name: 'leftChannel', defaultValue: 0 },
+    { name: 'rightChannel', defaultValue: 1 }];
   }
 
   constructor(dictionary) {
     super(dictionary);
-    this.channelCount = dictionary.channelCount;
-    this.leftOutput = 0;
+    // this.channelCount = dictionary.channelCount;
+    // this.numberOfInputs = dictionary.numberOfInputs;
+    // this.numberOfOutputs = dictionary.numberOfOutputs;
+    // this.outputChannelCount = dictionary.outputChannelCount;
   }
 
-  //TODO
-  //up mix mono signal to stereo
   process(inputs, outputs, parameters) {
-    let input = inputs[0];
-    //console.log("input . length in panx" + input.length);
-    let output = outputs[0];
+    let leftChannelParameter = parameters.leftChannel;
+    let rightChannelParameter = parameters.rightChannel;
+    let gain = parameters.gain;
 
-    let panParameter = parameters.panAudioParam; //.22
-    let rangePerSpeaker = 1.0 / output.length; //.03125
-    let pan = (panParameter[0] % rangePerSpeaker); //1.6
-    pan = pan / rangePerSpeaker;
+    let leftinput = inputs[0];
+    let rightinput = inputs[1];
+    leftinput = leftinput[0];
+    rightinput = rightinput[0];
+    
+    let leftoutput = outputs[Math.floor(leftChannelParameter[0])];
+    let rightoutput = outputs[Math.floor(rightChannelParameter[0])];
+    leftoutput = leftoutput[0];
+    rightoutput = rightoutput[0];
 
-    let speaker = Math.floor(panParameter[0] * output.length);
-    let nextSpeaker = speaker + 1;
-    if(this.leftOutput != speaker) { //switching primary stereo speaker pairs
-      pan = 0;
-      this.leftOutput = speaker;
-    }
-
-    // console.log("speaker" + speaker + "next" + nextSpeaker);
-    for (let channel = 0; channel < output.length; ++channel) {
-      if(channel == nextSpeaker)
-      {
-        let outputChannel = output[channel];
-        let inputChannel = input[0];
-        for (let i = 0; i < inputChannel.length; ++i) {
-          outputChannel[i] = inputChannel[i] * pan;
-        }
+    if (gain.length === 1)
+      for (let i = 0; i < leftinput.length; ++i) {
+        leftoutput[i] = leftinput[i];// * gain[0];
+        rightoutput[i] = rightinput[i];// * gain[0];
       }
-      else if(channel == speaker)
-      {
-        let outputChannel = output[channel];
-        let inputChannel = input[1];
-        for (let i = 0; i < inputChannel.length; ++i) {
-          outputChannel[i] = inputChannel[i] * (1.0 - pan);
-        }
-      } 
-    }
+    else
+      for (let i = 0; i < leftinput.length; ++i) {
+        leftoutput[i] = leftinput[i];// * gain[i];
+        rightoutput[i] = rightinput[i];// * gain[i];
+      }
     return true;
-  }
-}
+  }//process
+}//panX
 registerProcessor('panX', panX);
